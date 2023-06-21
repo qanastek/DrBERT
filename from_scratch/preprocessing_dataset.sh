2d47ae2f131fa817a13206d363c54384fc7c97a1
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2023-06-21 15:19:01 +0200

    Update preprocessing_dataset.sh

--- from_scratch/preprocessing_dataset.sh
+++ from_scratch/preprocessing_dataset.sh
@@ -1,4 +1,23 @@
-python preprocessing_dataset.py \
+#!/bin/bash
+#SBATCH --job-name=DrBERT
+#SBATCH --ntasks=1             # Nombre total de processus MPI
+#SBATCH --ntasks-per-node=1    # Nombre de processus MPI par noeud
+#SBATCH --hint=nomultithread   # 1 processus MPI par coeur physique (pas d'hyperthreading)
+#SBATCH --cpus-per-task=24
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=gpu_p2
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --time=19:00:00
+#SBATCH --output=./logs/%x_%A_%a.out
+#SBATCH --error=./logs/%x_%A_%a.err
+#SBATCH -A rtl@v100
+
+module purge
+module load pytorch-gpu/py3/1.11.0
+
+nvidia-smi
+
+srun python preprocessing_dataset.py \
     --model_type='camembert' \
     --tokenizer_name='./Tokenizer/' \
     --train_file='../data/corpus.txt' \
