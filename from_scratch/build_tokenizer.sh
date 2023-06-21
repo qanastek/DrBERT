e12718e992e55849fcdd52a423a2e830d3a7d188
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2023-06-21 15:19:29 +0200

    Update build_tokenizer.sh

--- from_scratch/build_tokenizer.sh
+++ from_scratch/build_tokenizer.sh
@@ -1,1 +1,20 @@
-python build_tokenizer.py
+#!/bin/bash
+#SBATCH --job-name=DrBERT
+#SBATCH --ntasks=1             # Nombre total de processus MPI
+#SBATCH --ntasks-per-node=1    # Nombre de processus MPI par noeud
+#SBATCH --hint=nomultithread   # 1 processus MPI par coeur physique (pas d'hyperthreading)
+#SBATCH --cpus-per-task=6
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=gpu_p2
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --time=19:00:00
+#SBATCH --output=./logs/%x_%A_%a.out
+#SBATCH --error=./logs/%x_%A_%a.err
+#SBATCH -A rtl@v100
+
+module purge
+module load pytorch-gpu/py3/1.11.0
+
+nvidia-smi
+
+srun python build_tokenizer.py
